07895f7af0219aaa4d8882b6b94bc70496c865ba
lavsexpert <lavrov-sergey@yandex.ru>
2020-12-27 01:31:49 +0300

    Добавлены виджеты Scaffold и AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text(
-          "Hello, Flutter",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.red, fontSize: 30),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("First App"),
         ),
       ),
     ),
