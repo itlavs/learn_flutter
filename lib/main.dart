1bbaab0395572b4cfc2db56987857f040d012f4b
lavsexpert <lavrov-sergey@yandex.ru>
2020-12-27 01:28:29 +0300

    Добавлен виджет MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Center(
-      child: Text(
-        "Hello, Flutter",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.red, fontSize: 50),
+    MaterialApp(
+      home: Center(
+        child: Text(
+          "Hello, Flutter",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.red, fontSize: 30),
+        ),
       ),
     ),
   );
