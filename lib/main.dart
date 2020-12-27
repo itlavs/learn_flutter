24c380e9661cd5d165aafcc34c18ca3f30896628
lavsexpert <lavrov-sergey@yandex.ru>
2020-12-28 00:30:35 +0300

    Проект разделен на 2 модуля и изменено имя проекта

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget/widget.dart';
 
-void main() => runApp(MyStatelessWidget());
+void main() => runApp(MyWidget());
 
-class MyStatelessWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.indigo,
-        appBar: AppBar(
-          title: Text("Stateless Widget"),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                LinearProgressIndicator(
-                  value: 25,
-                  backgroundColor: Colors.orange,
-                ),
-                Text(
-                  "25 %",
-                  style: TextStyle(color: Colors.orange, fontSize: 20),
-                ),
-                Text(
-                  "Press button to downloads",
-                  style: TextStyle(color: Colors.orange, fontSize: 20),
-                )
-              ],
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          child: Icon(Icons.file_download),
-        ),
-      ),
-    );
-  }
-}
+
---
+++ lib/widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        appBar: AppBar(
+          title: Text("Stateless Widget"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                LinearProgressIndicator(
+                  value: 25,
+                  backgroundColor: Colors.orange,
+                ),
+                Text(
+                  "25 %",
+                  style: TextStyle(color: Colors.orange, fontSize: 20),
+                ),
+                Text(
+                  "Press button to downloads",
+                  style: TextStyle(color: Colors.orange, fontSize: 20),
+                )
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          child: Icon(Icons.file_download),
+        ),
+      ),
+    );
+  }
+}
