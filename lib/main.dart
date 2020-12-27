ac1996e5a41bb8a5a586a45de6a84f264600bb29
lavsexpert <lavrov-sergey@yandex.ru>
2020-12-28 00:42:55 +0300

    Добавлено изменение состояния

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_widget/widget.dart';
 
 void main() => runApp(MyWidget());
--- lib/widget.dart
+++ lib/widget.dart
@@ -1,44 +1,11 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_widget/state.dart';
 
-class MyWidget extends StatelessWidget {
+class MyWidget extends StatefulWidget {
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.indigo,
-        appBar: AppBar(
-          title: Text("Stateless Widget"),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                LinearProgressIndicator(
-                  value: 25,
-                  backgroundColor: Colors.orange,
-                ),
-                Text(
-                  "25 %",
-                  style: TextStyle(color: Colors.orange, fontSize: 20),
-                ),
-                Text(
-                  "Press button to downloads",
-                  style: TextStyle(color: Colors.orange, fontSize: 20),
-                )
-              ],
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          child: Icon(Icons.file_download),
-        ),
-      ),
-    );
+  State<StatefulWidget> createState() {
+    return MyState();
   }
 }
