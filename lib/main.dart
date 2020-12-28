76da5fb6946e9080e824a331c344446ce25b8634
lavsexpert <lavrov-sergey@yandex.ru>
2020-12-28 03:01:37 +0300

    Добавлены шрифты

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -6,20 +7,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: "Marmelad"),
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Images"),
+          title: Text("Ресурсы: Картинки и шрифты"),
         ),
         body: Center(
-            child: Stack(
-              fit: StackFit.expand,
-              children: <Widget>[
-                Image(
-                  fit: BoxFit.fitHeight,
-                  image: AssetImage("assets/images/back.png"),
+          child: Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              Image(
+                fit: BoxFit.fitHeight,
+                image: AssetImage("assets/images/back.png"),
+              ),
+              Image.asset("assets/icons/icon.png"),
+              Positioned(
+                top: 6,
+                left: 16,
+                child: Text(
+                  "Надпись",
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.indigo,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "PoiretOne"),
                 ),
-                Image.asset("assets/icons/icon.png")
-              ],
+              )
+            ],
           ),
         ),
       ),
